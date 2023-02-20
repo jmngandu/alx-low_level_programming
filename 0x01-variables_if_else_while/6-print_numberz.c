3edb77ec4369c034ddb4a8af1c794f9909973cb2
jmngandu <jamesmngandu@gmail.com>
2023-02-20 11:10:46 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  digit numbers of base 10 starting
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
