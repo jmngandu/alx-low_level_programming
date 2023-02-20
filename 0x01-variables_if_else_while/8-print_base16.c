a18a08168dcce6b06aa808336c87e02828da5ceb
jmngandu <jamesmngandu@gmail.com>
2023-02-20 11:33:41 -0800

    unbeatable combination for success

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char la;
+	
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+	
+	putchar('\n');
+	return (0);
+}
