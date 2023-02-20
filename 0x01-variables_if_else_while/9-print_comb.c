3e1d88afbb219bad61cbd4d39605d59ce8f32874
jmngandu <jamesmngandu@gmail.com>
2023-02-20 11:41:50 -0800

    unbeatacle combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - possible combinations of single-digit numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int  i;
+
+	while (i < 10)
+	{
+		putchar(48 + i);
+		if (i != 9)
+		{
+                	putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
