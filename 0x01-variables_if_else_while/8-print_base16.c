079e61cc209464a63bed64805856289b5f54533b
jmngandu <jamesmngandu@gmail.com>
2023-02-20 11:37:12 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int num;
 	char la;
-	
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (la = 'a'; la <= 'f'; la++)
 		putchar(la);
-	
+
 	putchar('\n');
 	return (0);
 }
