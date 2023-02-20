7f17a11f96a407970df856ec0ee07c29ebd50d8b
jmngandu <jamesmngandu@gmail.com>
2023-02-20 10:59:52 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  digit numbers of base 10 starting from 0
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++);
+	printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
