acd385e24acbdf1b1c98e462965ddd1826b9730c
jmngandu <jamesmngandu@gmail.com>
2023-02-20 10:56:55 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'));
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
