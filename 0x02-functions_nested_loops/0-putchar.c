acb4ea1d4632a914229a9091a4a4dddb9b41be14
jmngandu <jamesmngandu@gmail.com>
2023-02-22 10:20:06 -0800

    Added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: prints '_putcher\n'
+ * Description: prints '_putchar\n'
  * Return: Always 0
  */
 int main(void)
 {
-	_putcher('_');
-	_putcher('p');
-	_putcher('u');
-	_putcher('t');
-	_putcher('c');
-	_putcher('h');
-	_putcher('e');
-	_putcher('r');
-	_putcher('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putcher(char c);
+int _putchar(char c);
+void print_alphabet(void);
 
 #endif
