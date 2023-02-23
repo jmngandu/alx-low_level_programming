c230f3890b579b819ae917417f179d40467ce1d3
jmngandu <jamesmngandu@gmail.com>
2023-02-23 03:08:24 +0300

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: The number of _ characters to be printed.
+ */
+
+void print_line(int n)
+{
+int line;
+
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+{
+putchar('_');
+}
+}
+putchar('\n');
+}
