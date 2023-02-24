697875181e7d537fb03f9a46d7b0c07c31bbfe0e
jmngandu <jamesmngandu@gmail.com>
2023-02-24 14:06:41 -0800

    function that prints an intege

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	print_number(98);
+	print_numbers(98);
 	_putchar('\n');
-	print_number(402);
+	print_numbers(402);
 	_putchar('\n');
-	print_number(1024);
+	print_numbers(1024);
 	_putchar('\n');
-	print_number(0);
+	print_numbers(0);
 	_putchar('\n');
-	print_number(-98);
+	print_numbers(-98);
 	 _putchar('\n');
 	return (0);
 }
