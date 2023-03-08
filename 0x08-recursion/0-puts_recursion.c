cbe7976c36c4175b102c6dc1199dbc1025ac65fd
jmngandu <jamesmngandu@gmail.com>
2023-03-08 01:30:49 -0800

    function that prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* 0-puts_recursion.c */
+void _puts_recursion(char *s);
+
+/* 1-print_rev_recursion.c */
+void _print_rev_recursion(char *s);
+
+/* 2-strlen_recursion.c */
+int _strlen_recursion(char *s);
+
+/* 3-factorial.c */
+int factorial(int n);
+
+/* 4-pow_recursion.c */
+int _pow_recursion(int x, int y);
+
+/* 5-sqrt_recursion.c */
+int _sqrt_recursion(int n);
+
+/* 6-is_prime_number.c */
+int is_prime_number(int n);
+
+/* 7-is_palindrome.c */
+int is_palindrome(char *s);
+
+/* 100-wildcmp.c */
+int wildcmp(char *s1, char *s2);
+
+#endif
