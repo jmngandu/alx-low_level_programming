f2bf0788db150ddda1db439d3682bd1e9c9472b6
jmngandu <jamesmngandu@gmail.com>
2023-03-13 09:45:53 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,39 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * str_concat - Concatenate (combine) two strings
- * @s1: First string
- * @s2: Second string
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
  *
- * Return: Pointer to new space in memory, NULL if it fails
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3, *empt;
-	int i, len1, len2, j;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-	empt = "";
 	if (s1 == NULL)
-		s1 = empt;
-	if (s2 == NULL)
-		s2 = empt;
-	i = len1 = len2 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	len2++;
-	s3 = malloc((len1 + len2) * sizeof(*s1));
-	if (s3 == NULL)
-		return (NULL);
-	i = 0;
-	while (i < len1)
-	{
-		s3[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j <= len2)
-	{
-		s3[i] = s2[j];
-		i++;
-		j++;
-	}
-	return (s3);
-}
-Footer
+		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
+}
