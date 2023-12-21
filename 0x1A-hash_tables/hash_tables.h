8a6f105cb1c73e540022f53f9a2cb0d2cd0a7811
jmngandu <jamesmngandu@gmail.com>
2023-12-21 23:06:56 +0300

    hash_tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -34,15 +31,6 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -79,18 +67,17 @@ typedef struct shash_table_s
 	shash_node_t *shead;
 	shash_node_t *stail;
 } shash_table_t;
-
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-
-int search_and_replace(shash_table_t *ht, const char *key, const char *value);
-shash_node_t *create_node(shash_table_t *ht, const char *key, const char *val);
-void print_in_order(const shash_table_t *ht, _Bool in_order);
-void insert_in_beginning(shash_table_t *ht, shash_node_t *new_node);
-void insert_in_middle_or_end(shash_table_t *ht, shash_node_t *new_node);
-
-#endif /* HASH_TABLES_H */
+#endif
